18c3b0e0a02e3889933d4835b3f711eb2366b7cb
Anthony Lee <hososlch@gmail.com>
2022-04-01 15:40:37 +0800

    fix: 2160

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits.rb
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits.rb
@@ -1,6 +1,6 @@
 # @param {Integer} num
 # @return {Integer}
 def minimum_sum(num)
-  digits = num.to_s.split('').map(&:to_i).sort
+  digits = num.to_s.chars.map(&:to_i).sort
   10 * digits.first(2).sum + digits.last(2).sum
 end
